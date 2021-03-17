56fdd074ad86824c76cd51b14e3825a5b33d4971
piksel-man231 <piksel-man231@yandex.ru>
2021-03-17 16:06:11 +0300

    Корректировка файлов

--- f14.py
+++ f14.py
@@ -1,11 +1,12 @@
 import math
 
-#n = int(input('Vvedite N'))
+# n = int(input('Vvedite N'))
+
 
 def f14(n):
 
     f_1 = 10
-    for i in range (1, n+1):
+    for i in range(1, n+1):
         f = (((1/53) * f_1 + math.cos(f_1)) - 60)
         f_1 = f
 
